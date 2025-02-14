96cf9a0157cf8130a2a8d2fcf47d639f1de32181
aemmikko <amalyn.kaikko@gmail.com>
2025-02-14 12:30:51 -0800

    finish draft of food game level

--- assets/js/adventureGame/GameLevelFood.js
+++ assets/js/adventureGame/GameLevelFood.js
@@ -10,13 +10,71 @@ class GameLevelFood {
         let width = GameEnv.innerWidth;
         let height = GameEnv.innerHeight;
 
-
-        const image_src_space = path + "/images/gamify/desert.png";
+        //getting the background
+        const image_src_space = path + "/images/gamify/space.png";
         const image_data_space = {
             name: 'space',
             greeting: "Good luck surviving in space! Find food to keep yourself fed!",
             src: image_src_space,
             pixels: {height: 580, width: 1038}
         }
+
+        //player data (you are an astronaut)
+        const sprite_src_astronaut = path + "/images/gamify/astronaut.png";
+        const ASTRO_SCALE_FACTOR = 5;
+        const sprite_data_astronaut = {
+        id: 'Astronaut',
+        src: sprite_src_astronaut,
+        SCALE_FACTOR: ASTRONAUT_SCALE_FACTOR,
+        STEP_FACTOR: 1000,
+        ANIMATION_RATE: 50,
+        INIT_POSITION: { x: 0, y: height - (height/ASTRONAUT_SCALE_FACTOR) }, 
+        pixels: {height: 384, width: 512},
+        orientation: {rows: 3, columns: 4 },
+        down: {row: 0, start: 0, columns: 3 },
+        left: {row: 2, start: 0, columns: 3 },
+        right: {row: 1, start: 0, columns: 3 },
+        up: {row: 3, start: 0, columns: 3 },
+        hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
+        keypress: { up: 87, left: 65, down: 83, right: 68 } // W, A, S, D
+        }
+
+        //collectable data - cake
+        const sprite_src_cake = path + "/images/gamify/gameCake.png";
+        const sprite_data_cake = {
+        id: 'Cake',
+        src: sprite_src_cake,
+        SCALE_FACTOR: 8,
+        ANIMATION_RATE: 50,
+        pixels: {height: 256, width: 352},
+        INIT_POSITION: { x: (width / 2), y: (height / 2)},
+        orientation: {rows: 8, columns: 11 },
+        down: {row: 5, start: 0, columns: 3 }, 
+        hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
+        }
+
+        //collectable data - burger
+        const sprite_src_burger = path + "/images/gamify/burger.png";
+        const sprite_data_burger = {
+        id: 'Burger',
+        src: sprite_src_burger,
+        SCALE_FACTOR: 8,
+        ANIMATION_RATE: 50,
+        pixels: {height: 256, width: 352},
+        INIT_POSITION: { x: (width / 2), y: (height / 2)},
+        orientation: {rows: 8, columns: 11 },
+        down: {row: 5, start: 0, columns: 3 },
+        hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
+        }
+
+        //classify objects
+        this.objects = [
+            { class: Background, data: image_data_space },
+            { class: Player, data: sprite_data_astronaut },
+            { class: Npc, data: sprite_data_cake },
+            { class: Npc, data: sprite_data_burger },
+        ];
     }
 }
+
+export default GameLevelFood;
